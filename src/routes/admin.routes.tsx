8be72dfc8d0cf37b12938b1981c5e95b9c80c7d8
rashedrk <rashedrk001@gmail.com>
2024-01-20 23:15:12 +0600

    refactor sidebar items from hardcode to dynamic

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,10 +3,17 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
+import { NavLink } from "react-router-dom";
 
 type TRoutes = {
   path: string;
   element: ReactNode;
+};
+
+type TAdminSidebarItem = {
+  key: string;
+  label: ReactNode;
+  children?: TAdminSidebarItem[];
 };
 
 const adminPaths = [
@@ -57,3 +64,27 @@ export const adminRoutes = adminPaths.reduce((acc: TRoutes[], item) => {
   return acc;
 }, []);
 
+export const adminSidebarItems = adminPaths.reduce(
+  (acc: TAdminSidebarItem[], item) => {
+    if (item.path && item.element) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+
+    return acc;
+  },
+  []
+);
