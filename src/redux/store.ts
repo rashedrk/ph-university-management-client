ae3f6df6b26aebd163f8c842872eb97e1f4dc1af
rashedrk <rashedrk001@gmail.com>
2024-01-22 23:22:19 +0600

    solve error: A non-serializable value was detected in an action

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './features/auth/authSlice';
 import { baseApi } from "./api/baseApi";
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
-    key:'auth',
+    key: 'auth',
     storage
 }
 
@@ -16,7 +16,12 @@ export const store = configureStore({
         [baseApi.reducerPath]: baseApi.reducer,
         auth: persistedAuthReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware)
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }).concat(baseApi.middleware)
 });
 
 export const persistor = persistStore(store);
