1d8ed2c19fdf8265e704c29e71799b7646c3fade
rashedrk <rashedrk001@gmail.com>
2024-01-24 21:36:28 +0600

    Retrieve New Access Token Using Refresh Token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:5000/api/v1',
@@ -17,8 +18,21 @@ const baseQuery = fetchBaseQuery({
 
 const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
     const result = await baseQuery(args, api, extraOptions);
-    console.log(result);
+    if(result?.error?.status === 401){
+        const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
+            method: 'POST',
+            credentials: 'include'
+        })
+        const result = await res.json();
+        const user = api.getState().auth.user;
+        api.dispatch(setUser({
+            user,
+            token: result.data.accessToken,
+        }))
+        
+    }
 
+    return result;
 }
 
 export const baseApi = createApi({
