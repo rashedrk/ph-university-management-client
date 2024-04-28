005f228e8d8ad653f00508f600973b5e4d62c399
rashedrk <rashedrk001@gmail.com>
2024-04-28 19:56:46 +0600

    semester registration added

--- src/pages/admin/courseManagement/SemesterRegistration.tsx
+++ src/pages/admin/courseManagement/SemesterRegistration.tsx
@@ -1,9 +1,75 @@
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForm from "../../../components/form/PhForm";
+import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PhSelect";
+import { toast } from "sonner";
+import { TResponse } from "../../../types/global.type";
+import { useAddRegisteredSemesterMutation } from "../../../redux/features/admin/courseManagement.api";
+import { useGetAcademicSemesterQuery } from "../../../redux/features/admin/academicManagement.api";
+import { semesterStatusOptions } from "../../../constants/semester";
+import PHDatePicker from "../../../components/form/PhDatePicker";
+import PHInput from "../../../components/form/PhInput";
+
 const SemesterRegistration = () => {
-    return (
-        <div>
-            This is SemesterRegistration
-        </div>
-    );
+  const [addRegisteredSemester] = useAddRegisteredSemesterMutation();
+  const { data: academicSemester } = useGetAcademicSemesterQuery([
+    { name: "sort", value: "year" },
+  ]);
+
+  const academicSemesterOptions = academicSemester?.data?.map((item) => ({
+    value: item._id,
+    label: `${item.name} ${item.year}`,
+  }));
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("creating...");
+
+    const semesterData = {
+      ...data,
+      minCredit: Number(data.minCredit),
+      maxCredit: Number(data.maxCredit),
+    };
+
+    // console.log(semesterData);
+
+    try {
+      const res = (await addRegisteredSemester(semesterData)) as TResponse<any>;
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("Semester added successfully", { id: toastId });
+      }
+      // console.log(res);
+    } catch (error) {
+      console.log(error);
+      toast.error("something went wrong", { id: toastId });
+    }
+
+    // console.log(semesterData);
+  };
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelect
+            label="Academic Semester"
+            name="academicSemester"
+            options={academicSemesterOptions}
+          />
+          <PHSelect
+            label="Status"
+            name="status"
+            options={semesterStatusOptions}
+          />
+          <PHDatePicker name="startDate" label="Start Date" />
+          <PHDatePicker name="endDate" label="End Date" />
+          <PHInput type="number" label="Min Credit" name="minCredit" />
+          <PHInput type="number" label="Max Credit" name="maxCredit" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default SemesterRegistration;
